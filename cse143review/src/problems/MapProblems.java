c3a9d61c9095ac990fff3a8839fb5645c204119c
hannahjanerose <105456716+hannahjanerose@users.noreply.github.com>
2023-04-08 22:59:01 -0700

    all problems completed

--- cse143review/src/problems/MapProblems.java
+++ cse143review/src/problems/MapProblems.java
@@ -2,6 +2,8 @@ package problems;
 
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * See the spec on the website for example behavior.
@@ -12,8 +14,15 @@ public class MapProblems {
      * Returns true if any string appears at least 3 times in the given list; false otherwise.
      */
     public static boolean contains3(List<String> list) {
-        // TODO replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        Map<String, Integer> storage = new HashMap<>();
+        for (String word : list) {
+            if (storage.containsKey(word)) {
+                storage.put(word, storage.get(word) + 1);
+            } else {
+                storage.put(word, 1);
+            }
+        }
+        return storage.containsValue(3);
     }
 
     /**
@@ -21,7 +30,12 @@ public class MapProblems {
      * A key-value pair exists in the output iff the same key-value pair exists in both input maps.
      */
     public static Map<String, Integer> intersect(Map<String, Integer> m1, Map<String, Integer> m2) {
-        // TODO replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        Map<String, Integer> result = new HashMap<>();
+        for (String key : m1.keySet()) {
+            if (m2.containsKey(key) && Objects.equals(m2.get(key), m1.get(key))) {
+                result.put(key, m1.get(key));
+            }
+        }
+        return result;
     }
 }
