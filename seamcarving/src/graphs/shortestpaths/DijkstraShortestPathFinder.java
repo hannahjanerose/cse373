e47f82a3f3601ccc25af12ba49e083eebf572ac4
maia <mxo@cs.washington.edu>
2023-05-08 13:49:01 -0700

    seamcarving: Add assignment skeleton

---
+++ seamcarving/src/graphs/shortestpaths/DijkstraShortestPathFinder.java
@@ -0,0 +1,46 @@
+package graphs.shortestpaths;
+
+import priorityqueues.ExtrinsicMinPQ;
+import priorityqueues.NaiveMinPQ;
+import graphs.BaseEdge;
+import graphs.Graph;
+
+
+
+import java.util.Map;
+
+/**
+ * Computes shortest paths using Dijkstra's algorithm.
+ * @see SPTShortestPathFinder for more documentation.
+ */
+public class DijkstraShortestPathFinder<G extends Graph<V, E>, V, E extends BaseEdge<V, E>>
+    extends SPTShortestPathFinder<G, V, E> {
+
+    protected <T> ExtrinsicMinPQ<T> createMinPQ() {
+        return new NaiveMinPQ<>();
+        /*
+        If you have confidence in your heap implementation, you can disable the line above
+        and enable the one below.
+         */
+        // return new ArrayHeapMinPQ<>();
+
+        /*
+        Otherwise, do not change this method.
+        We override this during grading to test your code using our correct implementation so that
+        you don't lose extra points if your implementation is buggy.
+         */
+    }
+
+    @Override
+    protected Map<V, E> constructShortestPathsTree(G graph, V start, V end) {
+        // TODO: replace this with your code
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    @Override
+    protected ShortestPath<V, E> extractShortestPath(Map<V, E> spt, V start, V end) {
+        // TODO: replace this with your code
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+}
