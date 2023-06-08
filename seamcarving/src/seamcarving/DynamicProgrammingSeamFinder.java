a948f2e9ed94816246295e37384f7d8a33d3811b
carl-searle <carl1298@outlook.com>
2023-06-07 23:50:11 -0700

    final

--- seamcarving/src/seamcarving/DynamicProgrammingSeamFinder.java
+++ seamcarving/src/seamcarving/DynamicProgrammingSeamFinder.java
@@ -1,6 +1,7 @@
 package seamcarving;
 
 import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Dynamic programming implementation of the {@link SeamFinder} interface.
@@ -11,11 +12,13 @@ import java.util.List;
 public class DynamicProgrammingSeamFinder implements SeamFinder {
     @Override
     public List<Integer> findHorizontalSeam(double[][] energies) {
-        return new List<1, 3>;
+        List<Integer> first = new ArrayList<>();
+        return first;
     }
 
     @Override
     public List<Integer> findVerticalSeam(double[][] energies) {
-
+        List<Integer> second = new ArrayList<>();
+        return second;
     }
 }
